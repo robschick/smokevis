03656267233fa335b0824a6b2bcdeaf7db6809a9
Rob Schick <rss5@st-andrews.ac.uk>
2016-06-15 10:56:56 +0100

    Add initial data for one smoker
    All I've done is gone to a geocoding website (http://www.gpsvisualizer.com/geocode) and picked out some candidate places of living and working, etc. Added these in as a csv file in data-raw.

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Master Locations for Smoking Simulation
+#'
+#' This contains a few different representative locations
+#' of where I work, live, and shop. It's to be used as
+#' baseline for simulating a realistic smoking profile of
+#' a candidate smoker. I should be able to add a few more
+#' but this works for now.
+'locations'
